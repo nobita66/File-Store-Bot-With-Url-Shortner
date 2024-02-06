12fc2e9f93abcdd5b95e6e17274da3b62d1bea7d
nobita66 <152915019+nobita66@users.noreply.github.com>
2024-02-07 00:11:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config(object):
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "https://publicearn.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "85d2cf5838d6c742c6a855eb514af076ea5c3790")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "695255327"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://raj91r:<C3YFhuq32HUtI6mS>@cluster0.np8pk.mongodb.net/?retryWrites=true&w=majority")
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://raj91r:C3YFhuq32HUtI6mS@cluster0.np8pk.mongodb.net/?retryWrites=true&w=majority")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002113088489")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002072841628"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
